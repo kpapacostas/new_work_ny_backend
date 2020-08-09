9ce27b8e675dd916cfc048904d256045f089196f
github username <github email address>
2020-08-09 15:12:22 -0400

    added origin to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,4 +3,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       origins "http://localhost:3001"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
+    allow do
+      origins "https://new-work-ny.herokuapp.com"
+      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
   end
